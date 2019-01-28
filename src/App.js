527f316c541a589b4b394491db2a48f7a10da0b7
Scott Chalfant <chalfantscott@Scotts-MacBook-Pro-2.local>
2019-01-27 22:57:12 -0500

    media query added and mostly to size

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from './NCR-Resources/Test/elements/logo.png';
+import GoMobile from './NCR-Resources/Test/elements/icon_mobile.png';
+import Hand from './NCR-Resources/Test/elements/icon_hand-money.png';
+import Auto from './NCR-Resources/Test/elements/icon_auto.png';
+import Hamburger from './NCR-Resources/Test/elements/icon_hamburger.png';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div class="logo">
+            <img src={Logo} alt="main-logo"/>
+          </div>
+          <div class="nav-bar">
+              <a href="#" className="menu">
+                <p>Menu</p><img src={Hamburger} id="hamburger" alt="menu" onClick={this.handleClick}/>
+              </a>
+              <div className="navbar-links">
+                <a href="#">Contract</a>
+                <a href="#">Rates</a>
+                <a href="#">Loans</a>
+              </div>
+          </div>
         </header>
+
+        <body>
+        <div class="tiles">
+
+          <div className="mobile_tile1">
+            <img src={GoMobile} alt="goMobile" />
+            <div className="content-tile">
+              <h2 className="title-tile">Go Mobile</h2>
+              <p>Download the App</p>
+            </div>
+          </div>
+
+          <div className="mobile_tile2">
+            <img src={Hand} alt="hand" />
+            <div className="content-tile">
+              <h2 className="title-tile">Refer Someone!</h2>
+              <p>You'll each get $25!</p>
+            </div>
+          </div>
+
+          <div className="mobile_tile3">
+            <img src={Auto} alt="auto" />
+            <div className="content-tile">
+              <h2 className="title-tile">Refinancing</h2>
+              <p>Get a low rate!</p>
+            </div>
+          </div>
+
+        </div>
+
+
+        </body>
       </div>
     );
   }
